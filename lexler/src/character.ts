0f1f0081149a7c1a581202961ec75066f0d2cbc8
Lada Kesseler <archie.kesseler@gmail.com>
2026-05-28 13:30:04 +0200

    feat(green): dealDamage reduces target health by amount

--- lexler/src/character.ts
+++ lexler/src/character.ts
@@ -2,5 +2,7 @@ export class Character {
   health = 1000;
   isAlive = true;
 
-  dealDamage(_target: Character, _amount: number): void {}
+  dealDamage(target: Character, amount: number): void {
+    target.health -= amount;
+  }
 }
